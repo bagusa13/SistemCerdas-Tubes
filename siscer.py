07b72c46542384bad1d5ef3a04b5781644ad8655
bagusa13 <bagusa13@gmail.com>
2026-06-05 00:45:48 +0700

    try2

--- siscer.py
+++ siscer.py
@@ -2,6 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-columns = ['Age', 'Op_Year', 'axil_nodes', 'Surv_status']
-src = "haberman.csv, haberman.data, haberman.txt"
-df = pd.read_csv(src, names=columns)
+df = pd.read_csv('haberman.csv')
+df.columns = ['age', 'op_Year', 'axil_nodes', 'surv_status']
+
+print("tampilkan baris pertama:")
+display(df.head())
+
+print("\nstatus kelangsungan hidup:")
+print("1 = bertahan hidup 5 tahun atau lebih\n2 = meninggal dalam 5 tahun")
+print(df['surv_status'].value_counts())
+
+
+plt.figure(figsize=(6, 4))
+sns.countplot(data=df, x='surv_status', palette='set2')
+plt.title('distribusi kelas (survival status)')
+plt.xlabel('Status (1 = >= 5 tahun, 2 = < 5 tahun)')
+plt.ylabel('jumlah Pasien')
+plt.show()
