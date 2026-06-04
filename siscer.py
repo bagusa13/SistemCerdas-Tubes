39f7da1b85196a428d9100ce92162f2a38b47c25
bagusa13 <bagusa13@gmail.com>
2026-06-05 00:35:14 +0700

    try1

--- siscer.py
+++ siscer.py
@@ -1,1 +1,7 @@
-print("CEK CEKK")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+columns = ['Age', 'Op_Year', 'axil_nodes', 'Surv_status']
+src = "haberman.csv, haberman.data, haberman.txt"
+df = pd.read_csv(src, names=columns)
